cfd771552fc94731a8fb2cea1b6c7d9f1e1a1aba
Adriana Gutierrez <Sierradaniels1@gmail.com>
2022-03-31 21:25:28 -0400

    addfiles

---
+++ todolist.js
@@ -0,0 +1,36 @@
+const addButton = document.querySelector('.addToDo')
+// console.log( addButton)
+const container = document.querySelector('.toDoContainer')
+// console.log ( container)
+const input = document.querySelector('.todo-input')
+// console.log(input)
+ 
+
+
+addButton.addEventListener('click', e => { 
+
+    const listItem = document.createElement('li')
+    listItem.innerText = input.value
+    container.appendChild(listItem)
+    input.value = ' ';
+    
+    
+        listItem.addEventListener( 'click', e=> { 
+            listItem.style.textDecoration = 'line-through'
+
+         
+    })
+        
+
+    listItem.addEventListener( 'dblclick', e=> { 
+        container.removeChild(listItem)
+
+
+    })
+
+})
+
+
+
+
+
